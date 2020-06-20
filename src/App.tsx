4628816e4c61a79592b268370516b16821102a59
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-06-20 18:51:52 +0900

    React + TypeScript: Displaying grids static

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const layoutAreaStyle = {
+	width: 500,
+	height: 400,
+	overflow: 'hidden',
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const boxSize = 20;
+	const boxStyle = {
+			width: boxSize,
+			height: boxSize,
+			boxShadow: 'inset -1px -1px #0275b8',
+			opacity: 0.4
+	};
+	const getBoxCount = () => {
+			const countX = Math.floor(layoutAreaStyle.width / boxSize) + 1;
+			const countY = Math.floor(layoutAreaStyle.height / boxSize) + 1;
+			return countX * countY;
+	};
+	const boxCount = getBoxCount();
+	return (
+			<div className="App">
+					<header className="App-header">
+					</header>
+					<main>
+							<div id="layout-area" style={layoutAreaStyle}>
+									<div id="grid" style={{ width: layoutAreaStyle.width + boxSize }}>
+											{Array.from(new Array(boxCount), (element, id) => (
+													<div style={boxStyle} key={id} />
+											))}
+									</div>
+							</div>
+					</main>
+			</div>
+	);
 }
 
 export default App;
